4e43e6f98f718cfae6922f7eae525057c0c4830e
Ashley Lee <72521813+ylee73@users.noreply.github.com>
2021-02-23 14:05:41 -0800

    Update with navigation instructions

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ This is Ashley Lee's rooms of my house p5.js that shows navigation structure usi
 ### Technical Details
 
 You may change the png. images to change the rooms or add in more states to interact with. 
+
+### Notes 
+
+(1) This code translates Ashley's dream house that was made in XD.
+	
+(2) Use left,right,down, and up keyboard button to navigate between rooms.
+
+(3) each state is a different room which has a label of the room and the navigation instructions.
--- sketch.js
+++ sketch.js
@@ -7,6 +7,8 @@
 	Note: 
 	(1) this code translates Ashley's dream house that was made in XD
 	(2) use left,right,down, and up keyboard button to navigate between rooms
+	(3) each state is a different room which has a label of the room and the 
+	navigation insturctions.
 ******************************************************************************/
 
 // array of images + placing
@@ -14,7 +16,7 @@ var images = [];
 var imagesX = 0;
 var imagesY = -5;
 
-// variable that is a function 
+// variable that is a draw function 
 var drawFunction;
 
 // offset from bottom of screen
@@ -26,9 +28,15 @@ var instructionsY = 350;
 var instructionsX = 110;
 var lineHeight = 24;
 
-// room labels
+//Navigation instructions per room;
+var stringsRoomInst = [];
+var roomInstX = 300; 
+var roomInstY = 300;
+
+// room labels 
 var labelsX = 530;
 var labelsY = 70;
+
 //load all images into an array
 function preload() {
 	images[0] = loadImage('AssetsHouse/opening.png');
@@ -66,12 +74,35 @@ function draw() {
   drawFunction();
 }
 
+// load instructions array
 function loadArray() {
 	strings[0] = "Ashley's Dream Home"
 	strings[1] = ""
 	strings[2] = "Press arrow keyboard to navigate"
 	strings[3] = "Look for the blue arrows"
 	strings[4] = "Press the up keyboard to start"
+
+}
+// load room instructions array
+function loadArrayRoomInstructions() {
+	// hallway
+	stringsRoomInst[0] = "Press left, right, or down"
+	// kitchen
+	stringsRoomInst[1] = "Press left, right, or down"
+	// laundry
+	stringsRoomInst[2] = "Press left"
+	// dining 
+	stringsRoomInst[3] = "Press left or up"
+	// living
+	stringsRoomInst[4] = "Press left, right, or up"
+	// living bathroom
+	stringsRoomInst[5] = "Press right"
+	// bedroom
+	stringsRoomInst[6] = "Press right or down"
+	// dressing
+	stringsRoomInst[7] = "Press up or down"
+	// bedroom bathroom
+	stringsRoomInst[8] = "Press left, right, or down"
 
 }
 // draw Opening
@@ -95,60 +126,94 @@ drawHallway = function() {
 	stroke(4);
 	textSize(25);
 	text("Hallway", labelsX, labelsY);
+
+	//room navigation instruction
+	loadArrayRoomInstructions();
+	text(stringsRoomInst[0], roomInstX, roomInstY - 50);
 }
 
 // draw Kitchen
 drawKitchen = function() {
 	image(images[6], imagesX, imagesY);
 	text("Kitchen", labelsX, labelsY);
+
+	//room navigation instruction
+	loadArrayRoomInstructions();
+	text(stringsRoomInst[1], roomInstX, roomInstY + 70);
 }
 
 // draw Bedroom
 drawBedroom = function() {
 	image(images[1], imagesX, imagesY);
 	text("Bedroom", labelsX, labelsY);
+
+	//room navigation instruction
+	loadArrayRoomInstructions();
+	text(stringsRoomInst[6], roomInstX, roomInstY);
 }
 
 // draw Bedroom Bathroom
 drawBedroomBathroom = function() {
 	image(images[2], imagesX, imagesY);
 	text("Bedroom Bathroom", labelsX - 90, labelsY);
+
+	//room navigation instruction
+	loadArrayRoomInstructions();
+	text(stringsRoomInst[8], roomInstX, roomInstY - 20);
 }
 
 // draw Dinging 
 drawDining= function() {
 	image(images[3], imagesX, imagesY);
 	text("Dining", labelsX, labelsY);
+
+	//room navigation instruction
+	loadArrayRoomInstructions();
+	text(stringsRoomInst[3], roomInstX, roomInstY + 150);
 }
 
 // draw Dressing
 drawDressing = function() {
 	image(images[4], imagesX, imagesY);
 	text("Dressing", labelsX, labelsY);
+
+	//room navigation instruction
+	loadArrayRoomInstructions();
+	text(stringsRoomInst[7], roomInstX, roomInstY);
 }
 
 // draw Laundry
 drawLaundry = function() {
 	image(images[7], imagesX, imagesY);
 	text("Laundry", labelsX, labelsY);
+
+	//room navigation instruction
+	loadArrayRoomInstructions();
+	text(stringsRoomInst[2], roomInstX, roomInstY);
 }
 
 // draw Living
 drawLiving= function() {
 	image(images[8], imagesX, imagesY);
 	text("Living", labelsX , labelsY);
+
+	//room navigation instruction
+	loadArrayRoomInstructions();
+	text(stringsRoomInst[4], roomInstX, roomInstY);
 }
 
 // draw Living Bathroom
 drawLivingBathroom= function() {
 	image(images[9], imagesX, imagesY);
 	text("Living Bathroom", labelsX - 90, labelsY);
+
+	//room navigation instruction
+	loadArrayRoomInstructions();
+	text(stringsRoomInst[5], roomInstX, roomInstY);
 }
 
 //Navigation functioncode 
 function keyPressed(){
-	//shows which was typed
-	print(keyCode);
 
 	// From Opening
 	if (drawFunction === drawOpening) {
